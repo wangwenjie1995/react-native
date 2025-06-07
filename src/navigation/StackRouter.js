dc47f215d8165726466d84bba577f0100c25f918
wagnwenjie <wagnwenjie1995@gmail.com>
2025-06-07 17:22:42 +0800

    feature: 添加/锁定插件版本

--- src/navigation/StackRouter.js
+++ src/navigation/StackRouter.js
@@ -1,10 +1,10 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { StatusBar, View, Text } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import {StatusBar, View, Text} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 const Stack = createNativeStackNavigator();
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
     </View>
   );
@@ -12,7 +12,7 @@ function HomeScreen() {
 function SafeScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{flex: 1}}>
         <StatusBar backgroundColor="#61dafb" />
         <View>
           <Text>SafeScreen</Text>
@@ -25,14 +25,8 @@ function SafeScreen() {
 export default function StackRouter() {
   return (
     <Stack.Navigator initialRouteName="SafeScreen">
-      <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-      />
-      <Stack.Screen
-          name="SafeScreen"
-          component={SafeScreen}
-      />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="SafeScreen" component={SafeScreen} />
     </Stack.Navigator>
   );
 }
